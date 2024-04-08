cada64bb1b84126988ad509bf17327b6b1283532
Muhammad Abdurrohman <90360993+Hade21@users.noreply.github.com>
2024-04-09 07:48:23 +0800

    feat: create sidebar (#17)

---
+++ constans/index.ts
@@ -0,0 +1,27 @@
+export const sidebarLinks = [
+    {
+        label: 'Home',
+        imgUrl: '/icons/Home.svg',
+        route: '/',
+    },
+    {
+        label: 'Upcoming',
+        imgUrl: '/icons/upcoming.svg',
+        route: '/upcoming',
+    },
+    {
+        label: 'Prevoius',
+        imgUrl: '/icons/previous.svg',
+        route: '/previous',
+    },
+    {
+        label: 'Recording',
+        imgUrl: '/icons/Video.svg',
+        route: '/recording',
+    },
+    {
+        label: 'Personal Room',
+        imgUrl: '/icons/add-personal.svg',
+        route: '/personal-room',
+    },
+]
